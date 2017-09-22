013249e5789c97d198553e0bc35ab35ea6bfab07
Kimberly Bordon <kbordon@gmail.com>
2017-09-22 10:10:09 -0700

    Add function replaceNumbers to address spec 3, 4 and 5.

--- js/scripts.js
+++ js/scripts.js
@@ -9,7 +9,11 @@ var countUpToNumber = function(userInteger) {
 
 var replaceNumbers = function(numbersArray) {
   var replacedNumbers = numbersArray.map(function(number){
-    if (number % 3 === 0) {
+    if (number % 15 === 0) {
+      return "ping-pong";
+    } else if (number % 5 === 0) {
+      return "pong";
+    } else if (number % 3 === 0) {
       return "ping";
     } else {
       return number
